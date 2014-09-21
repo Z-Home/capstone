51b2482fef465e950309f57f3f0b145f4d376c0c
Bryan <bryan.rich3@gmail.com>
2014-09-21 18:51:32 -0500

    moved url

--- connect/command.go
+++ connect/command.go
@@ -17,13 +17,13 @@ func (command *Command) ReadCommand(line string) {
 	var c Command
 	json.Unmarshal([]byte(line), &c)
 
-	command.SendCommand(c)
+	url := fmt.Sprintf("http://192.168.0.17:8083/ZWaveAPI/Run/devices[%s].instances[0].commandClasses[%s].Set(%s)", c.Device, c.CommandClass, c.Command)
+
+	command.SendCommand(url)
 }
 
-func (command *Command) SendCommand(c Command) {
-	a := fmt.Sprintf("http://192.168.0.17:8083/ZWaveAPI/Run/devices[%s].instances[0].commandClasses[%s].Set(%s)", c.Device, c.CommandClass, c.Command)
-
-	response, err := http.Get(a)
+func (command *Command) SendCommand(url string) {
+	response, err := http.Get(url)
 	if err != nil {
 		fmt.Printf("%s", err)
 		os.Exit(1)
