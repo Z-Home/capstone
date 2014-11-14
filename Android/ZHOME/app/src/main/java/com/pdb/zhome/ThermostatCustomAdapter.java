32289dacddecb804da22266c57036805ce79f0a6
brmqk3 <brmqk3@mail.missouri.edu>
2014-11-13 20:41:02 -0600

    Light count works, working on thermostat view

---
+++ Android/ZHOME/app/src/main/java/com/pdb/zhome/ThermostatCustomAdapter.java
@@ -0,0 +1,36 @@
+package com.pdb.zhome;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.miz.pdb.R;
+
+class ThermostatCustomAdapter extends ArrayAdapter<String> {
+    public ThermostatCustomAdapter(Context context, String[] values) {
+        super(context, R.layout.row_layout_thermostat, values);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater theInflater = LayoutInflater.from(getContext());
+
+        View theView = theInflater.inflate(R.layout.row_layout_thermostat, parent, false);
+
+        String favoriteItem = getItem(position);
+
+        TextView theTextView = (TextView) theView.findViewById(R.id.temperatureTxt);
+
+        theTextView.setText(favoriteItem);
+
+        ImageView theImageView = (ImageView) theView.findViewById(R.id.thermostatScreenImageView);
+
+        theImageView.setImageResource(R.drawable.thermostat);
+
+        return theView;
+    }
+}
